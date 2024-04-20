da3fd276bcae3095424d0d6b7652764d0e858a16
Jim Loza <jimloza25@gmail.com>
2024-04-20 12:56:27 -0600

    refactor(exception): add RcpExceptions

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   )
+  await app.listen();
   logger.log(`Products microservice running on port ${envs.port}`)
 
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -43,7 +44,12 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
   async findOne(id: number) {
     const data = await this.product.findUnique({ where: { id, available: true } })
-    if (!data) throw new NotFoundException(`Product with id: ${id} not found`)
+    if (!data) throw new RpcException(
+      {
+        message: `Product with id: ${id} not found`,
+        status: HttpStatus.BAD_REQUEST
+      }
+    )
     return data;
   }
 
